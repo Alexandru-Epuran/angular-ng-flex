30c361e773060a4f5704ab06819e60b27af566ce
Alexandru Epuran <epuranalex89@gmail.com>
2024-10-31 18:05:32 +0200

    implemented expert mode

--- src/app/simple/simple.component.ts
+++ src/app/simple/simple.component.ts
@@ -1,12 +1,15 @@
 import { Component } from "@angular/core";
 import { InputNumberComponent } from "../components/input-number/input-number.component";
 import { InputSwitchComponent } from "../components/input-switch/input-switch.component";
+import { CommonModule } from "@angular/common";
 
 @Component({
     selector: "app-simple",
     standalone: true,
-    imports: [InputNumberComponent, InputSwitchComponent],
+    imports: [InputNumberComponent, InputSwitchComponent, CommonModule],
     templateUrl: "./simple.component.html",
     styleUrl: "./simple.component.scss",
 })
-export class SimpleComponent {}
+export class SimpleComponent {
+    expertModeEnabled = false;
+}
